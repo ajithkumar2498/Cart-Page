7887296d3deba89116cb3761f3ba40cdc07409b9
ajithkumar2498 <ajibhc24@gmail.com>
2023-12-22 10:57:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Cart from './components/cart'
+import Context from './util/Context'
+
+
+function App() {
+
+
+  return (
+    <>
+    <Context>
+    <Cart/>
+    </Context>
+    </>
+  )
+}
+
+export default App
